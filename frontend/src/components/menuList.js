eaed1e8eecdb8e19c94d8ea9a86190e59d67c663
Vicky <vickyhl@ac.sce.ac.il>
2023-04-02 15:43:23 +0300

    Recipes menu

--- frontend/src/components/menuList.js
+++ frontend/src/components/menuList.js
@@ -1,28 +1,34 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
-import Menu from "./Menu";
-import { Link } from "react-router-dom";
 import "../components/tile.css";
 import axios from "axios";
 
 const MenuList = (props) => {
-  const [selectedMenuIndex, setSelectedMenuIndex] = useState(null);
-  const [menu, setMenu] = useState();
-  // let menu;
+  let userData = localStorage.getItem("user");
+  let userID = JSON.parse(userData)._id;
+  const [menus, setMenus] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get(
+          `http://localhost:5000/api/menus/fetchMenus/${userID}`
+        );
+        setMenus(result.data.identifers);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [userID]);
 
   const handleMenuSelect = async (index) => {
-    const res = await axios.get(
-      `http://localhost:5000/api/menus/${props.menus[index]}`
-    );
-    setMenu(res.data.menu);
-
-    setSelectedMenuIndex(index);
+    window.location.assign(`http://localhost:3000/menu/${menus[index]}`);
   };
 
   return (
     <React.Fragment>
-      {menu && <Menu items={menu} />}
-
+      {/* {menu && <Menu items={identifers[selectedMenuIndex]} />} */}
       {props.menus.map((menu, i) => (
         <div
           className="tile"
@@ -30,12 +36,10 @@ const MenuList = (props) => {
           data-index={i}
           onClick={() => handleMenuSelect(i)}
         >
-          {/* <Link to={`/menu/${props.menus[i]}`}> */}{" "}
           <div className="tile-content">
             <h3>Menu number {i + 1}</h3>
-            {/* <p>{moment(menu.substring(0, 10)).format("DD/MM/YYYY")}</p> */}
+            <p>{moment(menu.substring(0, 10)).format("DD/MM/YYYY")}</p>
           </div>
-          {/* </Link> */}
         </div>
       ))}
     </React.Fragment>
